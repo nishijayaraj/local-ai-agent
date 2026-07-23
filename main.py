5662d3ec7a246f06191f9bde0b89a71ab6250fda
HP <nishijayaraj@gmail.com>
2026-07-24 00:35:13 +0530

    Integrated Ollama 2.5 with langchain

---
+++ main.py
@@ -0,0 +1,16 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+model = OllamaLLM(model="qwen2.5")
+template = """
+You are an exeprt in answering questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is the question to answer: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+result = chain.invoke({"reviews": [], "question": "What is the best pizza in the town?"})
+print(result)
